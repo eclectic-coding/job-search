041574434136c6c5551a7fcc36bd7c500b6181c5
eclecticcoding <chuck@eclecticsaddlebag.com>
2022-04-29 18:06:23 -0400

    Bill put nav menu items

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -5,6 +5,33 @@
         <a href="/" class="flex h-full items-center text-xl">
           {{ company }}
         </a>
+
+        <nav class="ml-12 h-full">
+          <ul class="m-0 flex h-full list-none p-0">
+            <li class="h-full">
+              <a href="#" class="flex h-full items-center py-2.5">Teams</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="#" class="flex h-full items-center py-2.5">Locations</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="#" class="flex h-full items-center py-2.5">
+                Life at Eclectic Corp
+              </a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="#" class="flex h-full items-center py-2.5">
+                How we hire
+              </a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="#" class="flex h-full items-center py-2.5">Students</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="#" class="flex h-full items-center py-2.5">Jobs</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
