16d96447a7548b5f58252c46b84b2d15687dad36
eclecticcoding <chuck@eclecticsaddlebag.com>
2022-04-29 09:11:52 -0400

    Basic navbar and styling

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <h1 class="mt-8 text-2xl text-center">Hello world</h1>
+  <div>
+    <MainNav />
+    <h1 class="mt-8 text-2xl text-center">Hello world</h1>
+  </div>
 </template>
 
 <script>
+import MainNav from '@/components/MainNav';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    MainNav
+  }
 };
 </script>
